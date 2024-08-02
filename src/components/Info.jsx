fd44889599de154c052057c6a71040016b38ab52
Adam Eleraky <adam_eleraky@hotmail.com>
2024-08-02 12:25:56 +0200

    final touches added

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -8,8 +8,14 @@ export default function Info () {
                 <h1 className="info-name">Adam Eleraky</h1>
                 <h4 className="info-title">Fullstack Developer</h4>
                 <div className="info-btns">
-                    <button className="email-btn"><i class="fa-solid fa-envelope"></i> Email</button>
-                    <button className="linkedin-btn"><a href="https://www.linkedin.com/in/adam-eleraky-87a540162/" target="_blank"><i class="fa-brands fa-linkedin"></i> LinkedIn</a></button>  
+                    <button className="email-btn">
+                        <a href="mailto:adam_eleraky@hotmail.com" target="_blank"><i class="fa-solid fa-envelope"></i> Email
+                        </a>
+                    </button>
+                    <button className="linkedin-btn">
+                        <a href="https://www.linkedin.com/in/adam-eleraky-87a540162/" target="_blank"><i class="fa-brands fa-linkedin"></i> LinkedIn
+                        </a>
+                    </button>  
                 </div>            
             </div>
         </div>
